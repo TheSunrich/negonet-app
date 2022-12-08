7dd789d636cacf8c62f21764ebf347f0e10582e3
EzequielCalcanat <79470817+EzequielCalcanat@users.noreply.github.com>
2022-12-08 14:00:29 -0600

    Moment fix

--- src/private/MainPage.tsx
+++ src/private/MainPage.tsx
@@ -13,14 +13,13 @@ import getDay from "date-fns/getDay"
 import 'react-datepicker/dist/react-datepicker.css'
 import { format } from 'date-fns';
 import { Appointment } from '../models/AppointmentModel';
-moment.lang('es', {
+moment.locale('es', {
   months: 'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split('_'),
   monthsShort: 'Enero._Feb._Mar_Abr._May_Jun_Jul._Ago_Sept._Oct._Nov._Dec.'.split('_'),
   weekdays: 'Domingo_Lunes_Martes_Miercoles_Jueves_Viernes_Sabado'.split('_'),
   weekdaysShort: 'Dom._Lun._Mar._Mier._Jue._Vier._Sab.'.split('_'),
-  weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_')
+  weekdaysMin: 'Do_Lu_Ma_Mi_Ju_Vi_Sa'.split('_'),
 });
-
 
 const MainPage = () => {
   useEffect(() => {
